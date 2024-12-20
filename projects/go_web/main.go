ba446c5350dbb0e2108ef1552560f4edb11973c5
Ryan Acosta <ryan.acosta@gmail.com>
2024-12-20 09:18:43 -0600

    refactor: replace go web app using gorilla/mux with one using gin
    Replace go web app using gorilla/mux with one using gin

--- projects/go_web/main.go
+++ projects/go_web/main.go
@@ -4,21 +4,27 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 	"github.com/racosta/multi-language-bazel-monorepo/projects/go_hello_world"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
+func RootHandler(c *gin.Context) {
 	log.Println("Received request")
-	w.Write([]byte(go_hello_world.HelloWorld()))
+	c.JSON(http.StatusOK, gin.H{
+		"message": go_hello_world.HelloWorld(),
+	})
 }
 
 func main() {
-	r := mux.NewRouter()
-	// Routes consist of a path and a handler function
-	r.HandleFunc("/", YourHandler)
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/", RootHandler)
 
 	// Bind to a port and pass our router in
-	log.Println("Going to listen on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Println("Going to listen on port 8080")
+	log.Fatal(r.Run(":8080"))
 }
--- shell.nix
+++ shell.nix
@@ -16,6 +16,7 @@ pkgs.mkShell {
     difftastic
     eza
     go
+    golangci-lint
     jq
     just
     kondo
