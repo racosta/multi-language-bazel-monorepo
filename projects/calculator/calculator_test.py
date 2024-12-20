a8c1b879dadac597bd6bdd550654cc34fef9a2d6
Ryan Acosta <ryan.acosta@gmail.com>
2024-12-19 19:14:35 -0600

    chore: modify py_library import and use in unittest
    Modify py_library import and use in unittest

--- projects/calculator/calculator_test.py
+++ projects/calculator/calculator_test.py
@@ -1,11 +1,16 @@
 import unittest
-from projects.calculator.calculator import Calculator
+
+from calculator import Calculator
 
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
         c = Calculator()
         self.assertEqual(c.add(1, 2), 3)
+
+    def test_sum_negative(self):
+        c = Calculator()
+        self.assertEqual(c.add(-4, -11), -15)
 
 
 if __name__ == "__main__":
